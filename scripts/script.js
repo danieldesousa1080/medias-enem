21b4cebb8dae911f70a5b3629f1b1b64f7852549
Daniel de Sousa <danieldesousa1080@gmail.com>
2022-05-13 13:43:27 -0300

    Adicionado o Efeito Fade-out ao clicar o botão

--- scripts/script.js
+++ scripts/script.js
@@ -4,9 +4,25 @@ let content = document.querySelector('.content')
 let main = document.querySelector('.default-layout')
 let character = document.querySelector('.character')
 
+let screen1Content = [content,character]
+
 startButton.addEventListener("click",()=>{
     body.style.backgroundColor = '#F7BF42'
-    content.style.display = 'none'
+    content.classList.add("fade-out")
     main.style.backgroundColor ='#DE8327'
-    character.style.display = 'none'
+    character.classList.add("fade-out")
 })
+
+screen1Content.forEach((item)=>{
+    disappear(item)
+})
+
+
+function disappear (x) {
+    x.addEventListener("animationend",(ev)=>{
+        if (ev.type === "animationend"){
+            x.style.display = "none"
+        };
+    },false
+    )  
+}
